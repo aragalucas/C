c7cf4d082feec3ee71f5a662656d8b361d05921c
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-02-29 09:12:35 -0300

    Add files via upload

---
+++ matematica.c
@@ -0,0 +1,19 @@
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "");
+    float numero;
+    float numeroDois;
+    printf("Digite um numero: ");
+    scanf("%f",&numero);
+    printf("Digite outro numero: ");
+    scanf("%f",&numeroDois);
+    float soma = numero + numeroDois;
+    float subtracao = numero - numeroDois;
+    float multiplicacao = numero * numeroDois;
+    float divisao = numero / numeroDois;
+    printf("\na soma dos numeros deu: %.1f", soma);
+    printf("\na subtraçã dos numeros deu: %.1f", subtracao);
+    printf("\na multipicação dos numeros deu: %.1f", multiplicacao);
+    printf("\na divisão dos numeros deu: %.1f", divisao);
+    return 0;
+}
