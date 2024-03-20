c961f5fc0182eef8749d38ccaace1332609c6421
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-03-20 10:56:13 -0300

    Add files via upload

---
+++ array_06.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+  int vetor[5];
+  int i;
+  int menorNumero = 200;
+  int maiorNumero = 0;
+  printf("Digite os elementos do vetor:\n");
+  for (i = 0;i < 5; i++) {
+    printf("Elemento %d: ", i + 1);
+    scanf("%d", &vetor[i]);
+    if (menorNumero < vetor[i]) {
+      menorNumero = menorNumero;
+    } else {
+      menorNumero = vetor[i];
+    }
+    if (maiorNumero > i) {
+      maiorNumero = maiorNumero;
+    } else {
+      maiorNumero = vetor[i];
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    printf("Elemento %d: %d\n", i + 1, vetor[i]);
+  }
+  printf("maior numero: %d\n", maiorNumero);
+  printf("menor numero: %d\n", menorNumero);
+ return 0;
+}
