1ac0012dd930eac21442e60e8ed88b4f58a874e0
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-03-20 10:20:16 -0300

    Add files via upload

---
+++ array_04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int vetor[3];
+  int i;
+
+  printf("Digite as notas do vetor:\n");
+  for (i = 0;i < 3; i++) {
+    printf("nota %d: ", i + 1);
+    scanf("%d", &vetor[i]);
+  }
+printf("\n");
+  for (i = 0; i < 3; i++) {
+    printf("nota %d: %d\n", i + 1, vetor[i]);
+  }
+  int soma = vetor[0] + vetor[1] + vetor[2];
+  float media = soma / 3;
+  printf("A media das notas e: %.1f", media);
+ return 0;
+}
