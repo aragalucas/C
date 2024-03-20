528e04092a951d814ad6fe7eedf77e8f095bf72b
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-03-20 09:18:43 -0300

    Add files via upload

---
+++ array_02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int vetor[5];
+  int i;
+
+  printf("Digite os elementos do vetor:\n");
+  for (i = 0;i < 5; i++) {
+    printf("Elemento %d: ", i + 1);
+    scanf("%d", &vetor[i]);
+  }
+
+  for (i = 0; i < 5; i++) {
+    printf("Elemento %d: %d\n", i + 1, vetor[i]);
+  }
+ return 0;
+}
