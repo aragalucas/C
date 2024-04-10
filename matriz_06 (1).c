fcf6607811b994d799ec60980acc7e77d84a0738
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-04-10 08:19:32 -0300

    Add files via upload

---
+++ matriz_06 (1).c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <locale.h>
+int main (){ 
+    setlocale(LC_ALL, "");
+    char disciplinas[2][200];
+    float notas[2][3];
+    float soma = 0, media[4];
+    int i, j;
+    char situacao[200];
+    for (i = 0; i < 3; i++) {
+        printf("Digite o nome da %iª disciplina:\n", i+1);
+        scanf("%s",&disciplinas[i]);
+        for (j = 0; j < 2; j++) {
+            printf("Digite a %iª nota: ", j +1);
+            scanf("%f",&notas[i][j]);
+            soma += notas[i][j];
+        }
+        media[i] = soma / j;
+        soma = 0;
+        printf("\n");
+      }
+        for (i = 0; i < 3; i++) {
+            printf("%iª disciplina: %s \n", i+1, disciplinas[i]);
+            for (j = 0; j < 2; j++) {
+                printf("%iª nota: %.1f \n", j+1, notas[i][j]);
+            }
+            printf("Média da %iª disciplina: %.1f \n", i + 1, media[i]);
+          if (media[i] > 7) {
+            printf("aprovado\n");
+          }
+          else if (media[i] >= 5) {
+            printf("recuperação\n");
+          } else {
+            printf("reprovado\n");
+          }
+          printf("\n");
+        }
+      return 0;
+      }
